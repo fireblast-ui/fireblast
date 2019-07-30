f35b6021733c0367d8635a544016be346aa48c04
Olafur Arason <olafura@olafura.com>
2019-07-30 02:01:07 -0400

    Implement use for module

--- lib/exx_html.ex
+++ lib/exx_html.ex
@@ -3,6 +3,13 @@ defmodule ExxHtml do
   use Exx
   alias ExxHtml.Iolist
 
+  defmacro __using__(_opts) do
+    quote do
+      require ExxHtml
+      import ExxHtml
+    end
+  end
+
   def process_exx(exx, _) do
     {:safe, Iolist.to_iolist(exx)}
     |> IO.inspect()
