5f5bf97d5309f52f8672397e13d6da6553cab8a6
Olafur Arason <olafura@olafura.com>
2019-11-27 15:08:51 -0500

    wip

--- lib/fireblast/util.ex
+++ lib/fireblast/util.ex
@@ -1,6 +1,13 @@
 defmodule Fireblast.Util do
+
+  alias ExXml.{Element, Fragment}
+
   def map_to_quoted_map(map) when is_map(map) do
     do_map_to_quoted_map(map)
+  end
+
+  def walk_ex_xml(%struct{} = element, acc, fun) when struct in [Element, Fragment] and is_function(fun) do
+    do_walk_ex_xml(element, acc, fun)
   end
 
   defp do_map_to_quoted_map(map) when is_map(map) do
@@ -23,5 +30,16 @@ defmodule Fireblast.Util do
 
   defp do_map_to_quoted_map(other) do
     other
+  end
+
+  defp do_walk_ex_xml(%struct{} = element, acc, fun) when struct in [Element, Fragment] do
+    {new_element, new_acc} = fun.(element, acc)
+    {new_children, new_acc} = element.children |> Enum.reduce(new_acc, &do_walk_ex_xml(&1, &2, fun))
+
+    {Map.put(new_element, :children, new_children), new_acc}
+  end
+
+  defp do_walk_ex_xml(other, acc, fun) do
+    fun.(other, acc)
   end
 end
