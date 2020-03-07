162af3123052d4fc5c3e6307e67a3834da921165
Olafur Arason <olafura@olafura.com>
2020-03-07 14:55:35 +0100

    Compress iolist to match Phoenix templates

--- lib/fireblast/util.ex
+++ lib/fireblast/util.ex
@@ -3,6 +3,11 @@ defmodule Fireblast.Util do
     do_map_to_quoted_map(map)
   end
 
+  def compress_io_list(l) when is_list(l) do
+    Enum.reduce(l, [], &do_compress_io_list/2)
+    |> :lists.reverse()
+  end
+
   defp do_map_to_quoted_map(map) when is_map(map) do
     keyword_list =
       map
@@ -23,5 +28,13 @@ defmodule Fireblast.Util do
 
   defp do_map_to_quoted_map(other) do
     other
+  end
+
+  defp do_compress_io_list(a, [b | acc]) when is_binary(a) and is_binary(b) do
+    [b <> a | acc]
+  end
+
+  defp do_compress_io_list(a, acc) do
+    [a | acc]
   end
 end
